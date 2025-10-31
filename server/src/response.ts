3a7d03925812062598ea6694ffb32273b2e09651
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-31 17:07:06 +0000

    add `sessionDirty` to `Response`

--- server/src/response.ts
+++ server/src/response.ts
@@ -2,4 +2,11 @@ import { Response as OtterResponse } from "@otterhttp/app"
 
 import type { Request } from "./request"
 
-export class Response<Req extends Request = Request> extends OtterResponse<Req> {}
+export class Response<Req extends Request = Request> extends OtterResponse<Req> {
+  sessionDirty: boolean
+
+  constructor(request: Req) {
+    super(request)
+    this.sessionDirty = false
+  }
+}
